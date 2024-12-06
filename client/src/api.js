86c824e49811813e5b9d1ed285bd48e2c4769142
BearerOP <ankity1892003@gmail.com>
2024-12-06 23:32:45 +0530

    chore: update API base URL for production and remove local environment configuration

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base configuration for your API
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // Adjust to your backend URL
+  baseURL: 'https://roxiler-assignment-2-hcf2.onrender.com/api', // Adjust to your backend URL
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',

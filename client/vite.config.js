df547c64d44aca2336647503258257c73ac11758
BearerOP <ankity1892003@gmail.com>
2024-12-06 23:48:34 +0530

    fix: update API proxy target to point to production URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://roxiler-assignment-2-hcf2.onrender.com',
         changeOrigin: true,
         secure: false,
       }

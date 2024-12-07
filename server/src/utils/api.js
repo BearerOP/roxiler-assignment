ab228e5e71f0d8152a378835dcd1ac856d8863a8
BearerOP <ankity1892003@gmail.com>
2024-12-07 15:42:37 +0530

    refactor: update API variable name for clarity

--- server/src/utils/api.js
+++ server/src/utils/api.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
-const THIRD_PARTY_API = 'https://s3.amazonaws.com/roxiler.com/product_transaction.json';
+const seedingDataApi = 'https://s3.amazonaws.com/roxiler.com/product_transaction.json';
 
 const fetchTransactionData = async () => {
   try {
-    const response = await axios.get(THIRD_PARTY_API);
+    const response = await axios.get(seedingDataApi);
     return response.data;
   } catch (error) {
     throw new Error('Failed to fetch transaction data');

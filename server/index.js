74c049b06bea9a6a41934bf3c4d0273223b7bba7
BearerOP <ankity1892003@gmail.com>
2024-12-07 16:03:48 +0530

    feat: implement dynamic CORS configuration with allowed origins

--- server/index.js
+++ server/index.js
@@ -7,16 +7,33 @@ const db = require("./db/dbConnection");
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors(
-  'https://roxiler-assignment-4lvn.vercel.app',
-  'https://roxiler-assignment-2-hcf2.onrender.com',
-  'https://roxiler-assignment-one.vercel.app',
-  'http://localhost:3000',
-  'http://localhost:5173',
-  
-
-));
 app.use(express.json());
+
+const allowedOrigins = [
+  "https://roxiler-assignment-4lvn.vercel.app",
+  "https://roxiler-assignment-2-hcf2.onrender.com",
+  "https://roxiler-assignment-one.vercel.app",
+  "http://localhost:3000",
+  "http://localhost:5173",
+];
+
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("Requested Origin:", origin);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 db.connectDB();
 
